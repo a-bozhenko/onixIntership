6ddee013dca20674ef06eacdaf6387bca8d6904c
Andrey Bozhenko <taren91@gmail.com>
2021-09-19 14:21:12 +0300

    initial commit

--- OnixIntership/ViewController.swift
+++ OnixIntership/ViewController.swift
@@ -9,6 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    func sendGetRequest(url: String) -> Void {
+        guard let url = URL(string: url) else {return }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { data, response, error in
+            if let response = response {
+                print(response);
+            }
+            
+            guard let data = data else {return}
+            
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+                
+            } catch {
+                print(error)
+            }
+        }.resume();
+    }
+    
+    
+    @IBAction func Button(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var TextArea: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
